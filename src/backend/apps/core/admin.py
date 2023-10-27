e7d155fef5245a648e3da19b84ec3cb0c96f3cc3
monik <Alex.uma2007@yandex.ru>
2023-10-28 01:21:11 +0500

    models finished

--- src/backend/apps/core/admin.py
+++ src/backend/apps/core/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 
 from .models import *
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "first_name", "phone", "role")
     list_display_links = ("email",)
     list_filter = ("role",)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("user", "rating", "department")
+    list_display_links = ("user",)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("title", "created")
+    list_display_links = ("title",)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("company", "title", "created")
+    list_display_links = ("title",)
