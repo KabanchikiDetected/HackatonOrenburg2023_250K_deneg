1770884f84c729c8a8da4506abbd27478e7d58a7
monik <Alex.uma2007@yandex.ru>
2023-10-28 11:53:56 +0500

    api docs swagger

--- src/backend/api/users/urls.py
+++ src/backend/api/users/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path, re_path, include
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import *
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Kabanchiki API",
+        default_version='v1',
+        description="This is Kabanchiki API. Welcome :)",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
+    path(
+        'docs/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('users/<int:pk>/', UserDetailView.as_view(), name="user_detail"),
