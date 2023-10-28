4eee27ff583abe09e55cf31504053e384fb6fba2
monik <Alex.uma2007@yandex.ru>
2023-10-29 01:32:17 +0500

    hotfix for rating

--- src/backend/api/education/views.py
+++ src/backend/api/education/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 from .utils import *
 from .serializers import *
@@ -7,14 +8,13 @@ from .models import TestLog
 from apps.core.models import Employee
 
 
-# TODO: add rating
 class TestLogListAPIView(generics.ListCreateAPIView):
     queryset = TestLog.objects.all()
     serializer_class = TestLogSerializer
     permission_classes = (IsAuthenticated,)
     
     def post(self, request, *args, **kwargs):
-        employee = Employee.objects.get(user__pk=request.data["user"])
+        employee = get_object_or_404(Employee, user__pk=request.data["user"])
         correct, total = request.data["correct"], request.data["total"]
         rating = count_rating(employee.rating, correct, total)
         employee.rating = rating
