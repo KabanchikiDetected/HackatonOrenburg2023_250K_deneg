6be2cc62bcd9a4e994a132a671fe8c5514da05c6
monik <Alex.uma2007@yandex.ru>
2023-10-28 15:58:31 +0500

    static files, images for companies and employee status

--- src/backend/api/companies/serializers.py
+++ src/backend/api/companies/serializers.py
@@ -4,6 +4,8 @@ from apps.core.models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(read_only=True)
+    
     class Meta:
         model = Company
         fields = "__all__"
