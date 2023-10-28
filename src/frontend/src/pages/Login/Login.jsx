562fb049dc1e135a3c53482d4a670c03238cfaa1
FofuHupopo <fofuhupopo@gmail.com>
2023-10-28 12:24:29 +0500

    add system

--- src/frontend/src/pages/Login/Login.jsx
+++ src/frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from "react";
+import classes from "./Login.module.css";
+
 
 const Login = () => {
-    return ( 
-        <>
-            <h1>Login</h1>
-        </>
-    );
-}
- 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Создаем объект с данными для отправки на сервер
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      // Отправляем данные на сервер
+      const response = await fetch("http://127.0.0.1:8000/api/auth/token/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+
+      // Проверяем, что токен присутствует в ответе
+      if (response.ok && result.auth_token) {
+        // Сохраняем токен в localStorage
+        localStorage.setItem("token", result.auth_token);
+        // Дополнительные действия после успешной авторизации
+        // например, перенаправление на другую страницу
+      } else {
+        // Обрабатываем ошибку авторизации
+        console.log("Ошибка авторизации");
+      }
+    } catch (error) {
+      console.log("Ошибка сервера:", error);
+    }
+  };
+
+  return (
+    <form className={classes.loginForm} onSubmit={handleSubmit}>
+      <h2>Авторизация</h2>
+      <div className={classes.formGroup}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </div>
+      <div className={classes.formGroup}>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Войти</button>
+    </form>
+  );
+};
+
+
 export default Login;
--- src/frontend/src/pages/Signup/Signup.jsx
+++ src/frontend/src/pages/Signup/Signup.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './Signup.module.css';
 
 const Signup = () => {
-    return ( 
-        <>
-            <h1>Signup</h1>
-        </>
-    );
-}
- 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const userData = {
+      email,
+      password,
+      birthday,
+      username,
+      name,
+      surname,
+      phone
+    };
+
+    fetch('http://127.0.0.1:8000/api/auth/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Обработка ответа от сервера
+      console.log(data);
+    })
+    .catch(error => {
+      // Обработка ошибки
+      console.error(error);
+    });
+  };
+
+  return (
+    <form className={classes.registrationForm} onSubmit={handleSubmit}>
+      <h2>Регистрация</h2>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="username">Логин:</label>
+        <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="email">Почта:</label>
+        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="phone">Номер телефона:</label>
+        <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="name">Имя:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="surname">Фамилия:</label>
+        <input type="text" id="surname" value={surname} onChange={(e) => setSurname(e.target.value)} required />
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="birthday">День рождения:</label>
+        <input type="text" id="birthday" value={birthday} onChange={(e) => setBirthday(e.target.value)} pattern="\d{4}-\d{2}-\d{2}" required />
+        <small>Формат: YYYY-MM-DD</small>
+      </div>
+
+      <div className={classes.formGroup}>
+        <label htmlFor="password">Пароль:</label>
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      </div>
+
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
+};
+
 export default Signup;
