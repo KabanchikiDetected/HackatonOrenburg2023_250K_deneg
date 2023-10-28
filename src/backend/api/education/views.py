8aa7644f769403061ce3b397c409a440de18151b
monik <Alex.uma2007@yandex.ru>
2023-10-29 01:30:26 +0500

    rating for employees

--- src/backend/api/education/views.py
+++ src/backend/api/education/views.py
@@ -1,12 +1,22 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-
+from .utils import *
 from .serializers import *
 from .models import TestLog
+from apps.core.models import Employee
 
 
+# TODO: add rating
 class TestLogListAPIView(generics.ListCreateAPIView):
     queryset = TestLog.objects.all()
     serializer_class = TestLogSerializer
     permission_classes = (IsAuthenticated,)
+    
+    def post(self, request, *args, **kwargs):
+        employee = Employee.objects.get(user__pk=request.data["user"])
+        correct, total = request.data["correct"], request.data["total"]
+        rating = count_rating(employee.rating, correct, total)
+        employee.rating = rating
+        employee.save()
+        return super().post(request, *args, **kwargs)
